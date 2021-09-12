31653751e3a377aeb2d5910be47a32c739cace25
Rebekka Rustad <79134327+rebekkarustad@users.noreply.github.com>
2021-09-12 13:40:06 +0200

    contact

---
+++ js/contact.js
@@ -0,0 +1,60 @@
+const form = document.querySelector("#contactForm");
+const fullName = document.querySelector("#fullName");
+const fullNameError = document.querySelector("#fullNameError");
+
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+
+const address = document.querySelector("#address");
+const addressError = document.querySelector("#addressError");
+
+const valid = document.querySelector("#validate");
+
+function validateForm(event) {
+    event.preventDefault();
+
+    if (checkLength(fullName.value, 0) === true) {
+        fullNameError.style.display = "none";
+    } else {
+        fullNameError.style.display = "block";
+    }
+
+    if (checkLength(subject.value, 9) === true) {
+        subjectError.style.display = "none";
+    } else {
+        subjectError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+
+    if (checkLength(address.value, 24) === true) {
+        addressError.style.display = "none";
+    } else {
+        addressError.style.display = "block";
+    }
+
+    console.log("hello")
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatch = regEx.test(email);
+    return patternMatch;
+}
